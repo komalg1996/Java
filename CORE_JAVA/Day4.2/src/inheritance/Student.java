236efb8721e2debeff55e74e72ff271b8d821039
Komal Deepak Ghorpade <121026850+komalg2723@users.noreply.github.com>
2023-05-13 12:56:38 +0530

    eventOrg

--- CORE_JAVA/Day4.2/src/inheritance/Student.java
+++ CORE_JAVA/Day4.2/src/inheritance/Student.java
@@ -1,23 +1,23 @@
 package inheritance;
 
 public class Student extends Person{
-	private int geadYear;
+	private int gradYear;
 	private String courseName;
 	private double fees;
 	private int marks;
 	
-	public Student(String fn, String ln,int gradYear,String courseName,double fee,int marks)
+	public Student(String fn, String ln,int gradYear,String courseName,double fees,int marks)
 	{
 		super(fn,ln);//programmer must call explicitly the matching super class cotr
 		System.out.println("in student constr.....");
 		//sub class specific init
-		this.geadYear=geadYear;
+		this.gradYear=gradYear;
 		this.courseName=courseName;
 		this.fees=fees;
 		this.marks=marks;
 	}
 	public String toString() {
-		return "Student "+super.toString()+" year of passing "+geadYear+" course "+courseName+" fees "+fees+" marks"+marks;
+		return "Student "+super.toString()+" year of passing "+gradYear+" course "+courseName+" fees "+fees+" marks"+marks;
 	}
 
 	
