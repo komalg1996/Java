68219b9e6ca319c3a083483e263d076ef9d716ba
Komal Deepak Ghorpade <121026850+komalg2723@users.noreply.github.com>
2023-08-11 14:02:21 +0530

    .

--- SpringBoot/demoCrud/src/main/java/com/demo/test/dao/UserReop.java
+++
@@ -1,9 +0,0 @@
-package com.demo.test.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.demo.test.entities.User;
-
-public interface UserReop extends CrudRepository<User, Integer>{
-
-}
